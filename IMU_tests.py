6e445df543a330e0be6342310a0091576a8c353b
zimanko <zimanko@gmail.com>
2018-02-05 23:10:47 +0100

    let's test

--- IMU_tests.py
+++ IMU_tests.py
@@ -1,50 +1,38 @@
 import time
-from di_sensors.inertial_measurement_unit import InertialMeasurementUnit
 from di_sensors import BNO055
 import brickpi3
+import MURbot
 
 BP = brickpi3.BrickPi3()
-imu = InertialMeasurementUnit()
 BN = BNO055.BNO055()
 
-# Calibration:
-if BN.get_calibration_status().count(3) != 4:
-    while True:
-        BN.set_calibration(BN.get_calibration())
-        # print(BN.get_calibration_status())
-        if BN.get_calibration_status().count(3) == 4 and imu.read_linear_acceleration().count(0) == 3:
-            break
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.EV3_ULTRASONIC_CM)
+BP.set_sensor_type(BP.PORT_4, BP.SENSOR_TYPE.EV3_ULTRASONIC_CM)
 
+BP.set_motor_position_kp(BP.PORT_D, 120)
+BP.set_motor_position_kd(BP.PORT_D, 100)
 
-def move(power):
-    BP.set_motor_power(BP.PORT_B, power)
-    BP.set_motor_power(BP.PORT_C, -power)
-    # space = (4 - len(str(power))) * ' '
-    # print ('Motor power set to:' + space + str(power) + '  |  Voltage: ' + str(BP.get_voltage_battery()))
+while BN.get_calibration_status().count(3) != 4:
+    BN.set_calibration(BN.get_calibration())
+    # print(BN.get_calibration_status())
+    if BN.get_calibration_status().count(3) == 4:
+        break
 
-p = 0
-while p < 1:
-    move(40)
-    t = 0
-    while t < 10:
-        # Read the magnetometer, gyroscope, accelerometer, euler, and temperature values
-        accel = imu.read_linear_acceleration()
+Tilt_dist = 40
+Motor_power = 40
+Turn = 0
 
-        string_to_print = "Accelerometer X: {:.1f}  Y: {:.1f} Z: {:.1f}".format(accel[0], accel[1], accel[2])
-        print(string_to_print)
-        time.sleep(0.1)
-        t += 1
-    move(-40)
-    t = 0
-    while t < 10:
-        # Read the magnetometer, gyroscope, accelerometer, euler, and temperature values
-        accel = imu.read_linear_acceleration()
-
-        string_to_print = "Accelerometer X: {:.1f}  Y: {:.1f} Z: {:.1f}".format(accel[0], accel[1], accel[2])
-        print(string_to_print)
-        time.sleep(0.1)
-        t += 1
-    move(0)
-    p += 1
+while Turn < 5:
+    BP.set_motor_position(BP.PORT_D, 0)
+    while BP.get_sensor(BP.PORT_1) > Tilt_dist and BP.get_sensor(BP.PORT_4) > Tilt_dist:
+        MURbot.move(Motor_power)
+    MURbot.move(0)
+    time.sleep(0.5)
+    BP.set_motor_position(BP.PORT_D, 120)
+    MURbot.move(-Motor_power / 2)
+    time.sleep(1)
+    MURbot.move(0)
+    Motor_power = -1 * Motor_power
+    Turn += 1
 
 BP.reset_all()
