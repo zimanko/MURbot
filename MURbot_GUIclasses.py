11290d0842376ba2a6e7d84df27441909cdeda97
zimanko <zimanko@gmail.com>
2018-08-28 23:52:39 +0200

    changing the drawing method part 1

--- MURbot_GUIclasses.py
+++ MURbot_GUIclasses.py
@@ -5,12 +5,14 @@ import math
 import time
 
 '''Global variables'''
-MB_POS = [0, 0]                     #The x, y coordinate of MURbot
-TOP_LEFT_CANVAS_COORD = [0, 0]      #The x, y coordinate of the top left corner of the canvas
-CANVAS_W = 0                        #Canvas handler
-SCALE_W = 0                         #Sclaing scrollbar handler
-SCALE = 1                           #Scale value
-MB_VELOCITY = [0, 2]        #The velocity in y (heading) and x (perpendicular to heading) direction in m/s regarding regarding the IMU sensor
+MB_POS = [0, 0]                         #The x, y coordinate of MURbot
+MB_CANV_CRD = [0, 0]                    #X, y coordinates for drawing
+ENV_OFFSET = [0, 0]                     #Enviroment offset coordinates for drawing
+POS_PER_CANVAS = [0, 0]                 #MB_CANV_CRD / canvas dimensions -> positioning driver when the window size has changed
+CANVAS_W = 0                            #Canvas handler
+SCALE_W = 0                             #Sclaing scrollbar handler
+SCALE = 1                               #Scale value
+MB_VELOCITY = [0, 2]                    #The velocity in y (heading) and x (perpendicular to heading) in m/s regarding regarding the IMU sensor
 
 
 '''GUI Classes and Functions'''
@@ -69,6 +71,8 @@ class NavCanvas(TK.Canvas):
 
             observation = {'ID':     int(time.time()),
                            'MBpos':  (MB_POS[0], MB_POS[1]),
+                           'MBCanvCrd': MB_CANV_CRD,
+                           'DottOffset': ENV_OFFSET,
                            'RawRD':  test_data,
                            'Coords': coords_for_canvas}
 
@@ -77,18 +81,24 @@ class NavCanvas(TK.Canvas):
             redraw_canvas(event)
 
         def forward(event):
-            global MB_POS
-            MB_POS[0] += MB_VELOCITY[1] * math.cos(MF.HEADING)
-            MB_POS[1] -= MB_VELOCITY[1] * math.sin(MF.HEADING)
-            print('MBpos: ' + str(MB_POS))
+            global MB_POS, MB_CANV_CRD
+            xd = MB_VELOCITY[1] * math.cos(MF.HEADING)
+            yd = MB_VELOCITY[1] * math.sin(MF.HEADING)
+            MB_POS[0] += xd
+            MB_POS[1] -= yd
+            MB_CANV_CRD[0] += xd * SCALE
+            MB_CANV_CRD[1] += yd * SCALE
             redraw_canvas(event)
             canvas.update()
 
         def backward(event):
-            global MB_POS
-            MB_POS[0] -= MB_VELOCITY[1] * math.cos(MF.HEADING)
-            MB_POS[1] += MB_VELOCITY[1] * math.sin(MF.HEADING)
-            print('MBpos: ' + str(MB_POS))
+            global MB_POS, MB_CANV_CRD
+            xd = MB_VELOCITY[1] * math.cos(MF.HEADING)
+            yd = MB_VELOCITY[1] * math.sin(MF.HEADING)
+            MB_POS[0] -= xd
+            MB_POS[1] += yd
+            MB_CANV_CRD[0] -= xd * SCALE
+            MB_CANV_CRD[1] -= yd * SCALE
             redraw_canvas(event)
             canvas.update()
 
@@ -105,45 +115,48 @@ class NavCanvas(TK.Canvas):
             canvas.update()
 
         def redraw_canvas(event):
-            global TOP_LEFT_CANVAS_COORD
+            global MB_CANV_CRD, ENV_OFFSET, POS_PER_CANVAS
             canvas.delete('MURbot')
             canvas.delete('Env_dots')
+            canvas.delete('rec')
 
             #Establish an inner rectangle zone in the canvas. If the MB reaches the border the canvas scrolls automatically to keep it visiable
             border_thickness = 0.2  #in percentage of the canvas width and height
-            border_left = TOP_LEFT_CANVAS_COORD[0] + canvas.winfo_width() * border_thickness
-            border_right = TOP_LEFT_CANVAS_COORD[0] + canvas.winfo_width() * (1 - border_thickness)
-            border_up = int(TOP_LEFT_CANVAS_COORD[1] - canvas.winfo_height() * border_thickness * SCALE)
-            border_down = TOP_LEFT_CANVAS_COORD[1] - canvas.winfo_height() * (1 - border_thickness)
-            print(border_up)
+            border_left = canvas.winfo_width() * border_thickness
+            border_right = canvas.winfo_width() * (1 - border_thickness)
+            border_up = canvas.winfo_height() * border_thickness
+            border_down = canvas.winfo_height() * (1 - border_thickness)
+            canvas.create_rectangle(border_left, border_down, border_right, border_up, fill=None, outline='red', tags='rec')
 
             #Out_of_zone flag shows if the MB outside or not of the borders
-            if MB_POS[0] < border_left - 5 or MB_POS[0] > border_right + 5 or \
-                    MB_POS[1] > border_up + 5 or  MB_POS[1] < border_down - 5:
+            if MB_CANV_CRD[0] < border_left - 5 or MB_CANV_CRD[0] > border_right + 5 or \
+                    MB_CANV_CRD[1] < border_up - 5 or MB_CANV_CRD[1] > border_down + 5:
                 out_of_zone = True
             else:
                 out_of_zone = False
 
             #The canvas scrolls automatically only if the MB inside the borders to keep it visible
             if out_of_zone == False:
-                v = int(MB_VELOCITY[1] * SCALE)
-                if v < 1:
-                    v = 1
-                if MB_POS[0] < border_left:
-                    canvas.xview_scroll(-v, TK.UNITS)
-                    TOP_LEFT_CANVAS_COORD[0] = TOP_LEFT_CANVAS_COORD[0] - v
-                if MB_POS[0] > border_right:
-                    canvas.xview_scroll(v, TK.UNITS)
-                    TOP_LEFT_CANVAS_COORD[0] = TOP_LEFT_CANVAS_COORD[0] + v
-                if MB_POS[1] > border_up:
-                    canvas.yview_scroll(-v, TK.UNITS)
-                    TOP_LEFT_CANVAS_COORD[1] = TOP_LEFT_CANVAS_COORD[1] + v
-                if MB_POS[1] < border_down:
-                    canvas.yview_scroll(v, TK.UNITS)
-                    TOP_LEFT_CANVAS_COORD[1] = TOP_LEFT_CANVAS_COORD[1] - v
+                if MB_CANV_CRD[0] < border_left:
+                    d = border_left - MB_CANV_CRD[0]
+                    MB_CANV_CRD[0] += d
+                    ENV_OFFSET[0] += d
+                if MB_CANV_CRD[0] > border_right:
+                    d = MB_CANV_CRD[0] - border_right
+                    MB_CANV_CRD[0] -= d
+                    ENV_OFFSET[0] -= d
+                if MB_CANV_CRD[1] < border_up:
+                    d = border_up - MB_CANV_CRD[1]
+                    MB_CANV_CRD[1] += d
+                    ENV_OFFSET[1] += d
+                if MB_CANV_CRD[1] > border_down:
+                    d = MB_CANV_CRD[1] - border_down
+                    MB_CANV_CRD[1] -= d
+                    ENV_OFFSET[1] -= d
 
-            MB_canvas_coords = [MB_POS[0] * SCALE, MB_POS[1] * SCALE]
-            draw_murbot(MB_canvas_coords)
+            POS_PER_CANVAS[0] = MB_CANV_CRD[0] / canvas.winfo_width()
+            POS_PER_CANVAS[1] = MB_CANV_CRD[1] / canvas.winfo_height()
+            draw_murbot(MB_CANV_CRD)
             env_dots(MF.RADARDATA)
 
 
@@ -158,14 +171,14 @@ class NavCanvas(TK.Canvas):
             murbot_coords[3] = int(radius2 * math.sin((angle[1] * math.pi) + MF.HEADING))
             murbot_coords[4] = int(radius2 * math.cos((angle[2] * math.pi) + MF.HEADING))
             murbot_coords[5] = int(radius2 * math.sin((angle[2] * math.pi) + MF.HEADING))
-            canvas.create_polygon(coords[0] + murbot_coords[0], -coords[1] + murbot_coords[1],
-                                  coords[0] + murbot_coords[2], -coords[1] + murbot_coords[3],
-                                  coords[0] + murbot_coords[4], -coords[1] + murbot_coords[5],
+            canvas.create_polygon(coords[0] + murbot_coords[0], coords[1] + murbot_coords[1],
+                                  coords[0] + murbot_coords[2], coords[1] + murbot_coords[3],
+                                  coords[0] + murbot_coords[4], coords[1] + murbot_coords[5],
                                   fill='light green',
                                   outline='green',
                                   tags='MURbot')
-            canvas.create_oval(coords[0] + 5 * SCALE, -coords[1] + 5 * SCALE,
-                               coords[0] - 5 * SCALE, -coords[1] - 5 * SCALE,
+            canvas.create_oval(coords[0] + 5 * SCALE, coords[1] + 5 * SCALE,
+                               coords[0] - 5 * SCALE, coords[1] - 5 * SCALE,
                                outline='green',
                                tags='MURbot')
 
@@ -192,39 +205,34 @@ class NavCanvas(TK.Canvas):
                 n += 1
 
         def scroll_right(event):
-            global TOP_LEFT_CANVAS_COORD
+            global DRAWING_CTRL
             canvas.xview_scroll(1, TK.UNITS)
-            TOP_LEFT_CANVAS_COORD[0] = TOP_LEFT_CANVAS_COORD[0] + 1
+            DRAWING_CTRL[0] = DRAWING_CTRL[0] + 1
 
         def scroll_left(event):
-            global TOP_LEFT_CANVAS_COORD
+            global DRAWING_CTRL
             canvas.xview_scroll(-1, TK.UNITS)
-            TOP_LEFT_CANVAS_COORD[0] = TOP_LEFT_CANVAS_COORD[0] - 1
+            DRAWING_CTRL[0] = DRAWING_CTRL[0] - 1
 
         def scroll_up(event):
-            global TOP_LEFT_CANVAS_COORD
+            global DRAWING_CTRL
             canvas.yview_scroll(-1, TK.UNITS)
-            TOP_LEFT_CANVAS_COORD[1] = TOP_LEFT_CANVAS_COORD[1] + 1
+            DRAWING_CTRL[1] = DRAWING_CTRL[1] + 1
 
         def scroll_down(event):
-            global TOP_LEFT_CANVAS_COORD
+            global DRAWING_CTRL
             canvas.yview_scroll(1, TK.UNITS)
-            TOP_LEFT_CANVAS_COORD[1] = TOP_LEFT_CANVAS_COORD[1] - 1
+            DRAWING_CTRL[1] = DRAWING_CTRL[1] - 1
 
         def reorganize_canvas(event):
-            global TOP_LEFT_CANVAS_COORD
-            canvas.xview_moveto(0)
-            canvas.yview_moveto(0)
-            TOP_LEFT_CANVAS_COORD = [0, 0]
-            canvas.xview_scroll(int(canvas.winfo_width() / -2), TK.UNITS)
-            canvas.yview_scroll(int(canvas.winfo_height() / -2), TK.UNITS)
-            TOP_LEFT_CANVAS_COORD[0] = canvas.winfo_width() / -2
-            TOP_LEFT_CANVAS_COORD[1] = canvas.winfo_height() / 2
+            global MB_CANV_CRD
+            MB_CANV_CRD[0] = canvas.winfo_width() * POS_PER_CANVAS[0]
+            MB_CANV_CRD[1] = canvas.winfo_height() * POS_PER_CANVAS[1]
+            redraw_canvas(event)
 
-        canvas.xview_scroll(int(canvas.winfo_width() / -2), TK.UNITS)
-        canvas.yview_scroll(int(canvas.winfo_height() / -2), TK.UNITS)
-        TOP_LEFT_CANVAS_COORD[0] = TOP_LEFT_CANVAS_COORD[0] - int(canvas.winfo_width() / 2)
-        TOP_LEFT_CANVAS_COORD[1] = TOP_LEFT_CANVAS_COORD[1] + int(canvas.winfo_height() / 2)
+
+        MB_CANV_CRD[0] += int(canvas.winfo_width() / 2)
+        MB_CANV_CRD[1] += int(canvas.winfo_height() / 2)
 
         scale.bind('<Motion>', redraw_canvas)
         scale.pack(pady=20, side='top')
